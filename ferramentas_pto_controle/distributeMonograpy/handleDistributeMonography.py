a4e71b94f82fc4d7eb474f1a6975ea0ab8ecb7bb
kretzer <bechtold.thiago@ime.eb.br>
2025-10-28 16:32:02 -0300

    opção de seleção de feições e monografia gerada dos atributos da camada

--- ferramentas_pto_controle/distributeMonograpy/handleDistributeMonography.py
+++ ferramentas_pto_controle/distributeMonograpy/handleDistributeMonography.py
@@ -1,7 +1,5 @@
 import os
 from pathlib import Path
-import psycopg2
-import psycopg2.extras
 import re
 from qgis.core import (
     QgsExpressionContextUtils,
@@ -11,10 +9,13 @@ from qgis.core import (
     QgsReadWriteContext,
 )
 from qgis.PyQt.QtXml import QDomDocument
+from datetime import timedelta, datetime
 
 class HandleDistributeMonography:
-    def __init__(self, conn, path, template_qpt, settings=None, digital=True, feedback=None, tipo_modelo=None):
-        self.conn = conn
+    def __init__(self, layer, feicao, path, template_qpt, settings=None, digital=True, feedback=None, tipo_modelo=None):
+        # self.conn = conn
+        self.layer = layer
+        self.feicao = feicao
         self.path = Path(path)
         self.template_qpt = template_qpt
         self.settings = settings or {}
@@ -29,14 +30,54 @@ class HandleDistributeMonography:
         else:
             print(msg)
 
+    # def fetchPointData(self, cod_ponto):
+    #     with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+    #         cur.execute(PointDataQueries.FETCH_POINT_WITH_DOMAINS, (cod_ponto,))
+    #         return cur.fetchone()
+
     def fetchPointData(self, cod_ponto):
-        with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            cur.execute(PointDataQueries.FETCH_POINT_WITH_DOMAINS, (cod_ponto,))
-            return cur.fetchone()
+        """Busca o ponto na camada de entrada com base no código do ponto."""
+        if self.feicao:
+            for feature in self.layer.getSelectedFeatures():
+                if str(feature['cod_ponto']) == str(cod_ponto):
+                    # Converte atributos em dicionário
+                    ponto = feature.attributes()
+                    fields = [f.name() for f in self.layer.fields()]
+                    ponto_dict = dict(zip(fields, ponto))
+                    return ponto_dict
+            self.log(f"Ponto {cod_ponto} não encontrado na camada.")
+            return None
+        if not self.feicao:
+            for feature in self.layer.getFeatures():
+                if str(feature['cod_ponto']) == str(cod_ponto):
+                    # Converte atributos em dicionário
+                    ponto = feature.attributes()
+                    fields = [f.name() for f in self.layer.fields()]
+                    ponto_dict = dict(zip(fields, ponto))
+                    return ponto_dict
+            self.log(f"Ponto {cod_ponto} não encontrado na camada.")
+            return None
 
     def getFoldersFromStructure(self):
-        folders = [x for x in self.path.rglob(
-            '*') if x.is_dir() and re.match(r'\w\w-\w\w-0*\d+', x.parts[-1])]
+        if self.feicao:
+            features = self.layer.getSelectedFeatures()
+        else:
+            features = self.layer.getFeatures()
+        valid_cods = set()
+        for feature in features:
+            cod = str(feature['cod_ponto'])
+            valid_cods.add(cod)
+            
+        folders = []
+        folder_name_pattern = re.compile(r'(\w\w-\w\w-0*\d+)$')
+        for x in self.path.rglob('*'):
+            if x.is_dir():
+                folder_name = x.parts[-1]
+                match = folder_name_pattern.match(folder_name)
+                if match:
+                    cod_from_folder = match.group(1) 
+                    if cod_from_folder in valid_cods:
+                        folders.append(x)            
         return folders
 
     def executeProcess(self, folder, tipo_modelo=None):
@@ -45,17 +86,106 @@ class HandleDistributeMonography:
         if not ponto:
             self.log(f"Ponto {cod_ponto} não encontrado no banco.")
             return
-        ponto['durrast'] = ponto["fim_rastreio"] - ponto["inicio_rastreio"]
-        ponto['inicio_rastreio'] = ponto['inicio_rastreio'].strftime('%d/%m/%Y %H:%M:%S')
-        ponto['data_processamento'] = ponto['data_processamento'].strftime('%d/%m/%Y')
+        DOMINIOS = {
+            "orbita": {
+                1: "Ultra Rápida (predita)",
+                2: "Ultra Rápida (observada)",
+                3: "Rápida",
+                4: "Final",
+                97: "Não aplicável",
+                9999: "A SER PREENCHIDO",
+            },
+            "metodo_posicionamento": {
+                1: "Posicionamento por ponto preciso (PPP)",
+                2: "Real Time Kinematic (RTK)",
+                3: "Semi-cinemático",
+                4: "Relativo Estático",
+                5: "Relativo Cinemático",
+                6: "Absoluto",
+                9999: "A SER PREENCHIDO",
+            },
+            "sistema_geodesico": {
+                1: "SAD-69",
+                2: "SIRGAS2000",
+                3: "WGS-84",
+                4: "Córrego Alegre",
+                5: "Astro Chuá",
+                6: "SAD-69 (96)",
+                99: "Outra referência",
+                9999: "A SER PREENCHIDO",
+            },
+            "referencia_medicao_altura": {
+                1: "Nível do solo",
+                2: "Nível do objeto",
+                9999: "A SER PREENCHIDO",
+            },
+            "referencial_altim": {
+                1: "Torres",
+                2: "Imbituba",
+                3: "Santana",
+                99: "Outra referência",
+                9999: "A SER PREENCHIDO",
+            },
+            "tipo_ref": {
+                1: "Altimétrico",
+                2: "Planimétrico",
+                3: "Planialtimétrico",
+                4: "Gravimétrico",
+                9999: "A SER PREENCHIDO",
+            },
+        }
+
+        def traduz_dominios(ponto):
+            for campo, mapa in DOMINIOS.items():
+                val = ponto.get(campo)
+                if val in mapa:
+                    ponto[campo] = mapa[val]
+            return ponto
+        
+        ponto = traduz_dominios(ponto)
+
+        def to_datetime(value):
+            """Converte valor para datetime Python, aceitando str, QDate, QDateTime ou datetime."""
+            if value is None:
+                return None
+            try:
+                # QDateTime
+                if hasattr(value, 'toPyDateTime'):
+                    return value.toPyDateTime()
+                # QDate
+                elif hasattr(value, 'toPyDate'):
+                    return datetime.combine(value.toPyDate(), datetime.min.time())
+                # Nenhum tipo reconhecido
+                else:
+                    print(f"Tipo desconhecido: {type(value)} -> {value}")
+                    return None
+
+            except Exception as e:
+                print(f"Erro ao converter valor {value} ({type(value)}): {e}")
+                return None
+            
+        inicio = to_datetime(ponto.get("inicio_rastreio"))
+        fim = to_datetime(ponto.get("fim_rastreio"))
+        proc = to_datetime(ponto.get("data_processamento"))
+
+        try:
+            if inicio and fim:
+                ponto['durrast'] = fim - inicio
+            else:
+                ponto['durrast'] = timedelta(0)
+            ponto['inicio_rastreio'] = inicio.strftime('%d/%m/%Y %H:%M:%S') if inicio else ''
+            ponto['data_processamento'] = proc.strftime('%d/%m/%Y') if proc else ''
+        except Exception as e:
+            self.log(f"Erro ao converter datas: {e}")
+            ponto['durrast'] = ''
+
+
         if ponto['materializado']:
             ponto['materializado'] = 'Sim'
         else:
             ponto['materializado'] = 'Não'
 
         self.preparePhotos(ponto, folder)
-
-        
 
         project = QgsProject.instance()
         layout_name = f"Monografia_{cod_ponto}"
@@ -207,27 +337,27 @@ class HandleDistributeMonography:
             except Exception as e:
                 self.log(f" Erro ao aplicar imagem em {item_id}: {e}")
 
-class PointDataQueries:    
-    FETCH_POINT_WITH_DOMAINS = """
-        SELECT *,
-            dominios.tipo_ref.code_name as tipo_ref1,
-            dominios.metodo_posicionamento.code_name as metodo_pos,
-            dominios.referencial_altim.code_name as referencial_altim1,
-            dominios.sistema_geodesico.code_name as sistema_geodesico1,
-            dominios.orbita.code_name as orbita1,
-            dominios.referencia_medicao_altura.code_name as refalt
-        FROM bpc.ponto_controle_p
-        INNER JOIN dominios.tipo_ref 
-            ON dominios.tipo_ref.code = bpc.ponto_controle_p.tipo_ref
-        INNER JOIN dominios.metodo_posicionamento 
-            ON dominios.metodo_posicionamento.code = bpc.ponto_controle_p.metodo_posicionamento
-        INNER JOIN dominios.referencial_altim 
-            ON dominios.referencial_altim.code = bpc.ponto_controle_p.referencial_altim
-        INNER JOIN dominios.sistema_geodesico 
-            ON dominios.sistema_geodesico.code = bpc.ponto_controle_p.sistema_geodesico
-        INNER JOIN dominios.orbita 
-            ON dominios.orbita.code = bpc.ponto_controle_p.orbita
-        INNER JOIN dominios.referencia_medicao_altura 
-            ON dominios.referencia_medicao_altura.code = bpc.ponto_controle_p.referencia_medicao_altura
-        WHERE cod_ponto = %s
-    """
+# class PointDataQueries:    
+#     FETCH_POINT_WITH_DOMAINS = """
+#         SELECT *,
+#             dominios.tipo_ref.code_name as tipo_ref1,
+#             dominios.metodo_posicionamento.code_name as metodo_pos,
+#             dominios.referencial_altim.code_name as referencial_altim1,
+#             dominios.sistema_geodesico.code_name as sistema_geodesico1,
+#             dominios.orbita.code_name as orbita1,
+#             dominios.referencia_medicao_altura.code_name as refalt
+#         FROM bpc.ponto_controle_p
+#         INNER JOIN dominios.tipo_ref 
+#             ON dominios.tipo_ref.code = bpc.ponto_controle_p.tipo_ref
+#         INNER JOIN dominios.metodo_posicionamento 
+#             ON dominios.metodo_posicionamento.code = bpc.ponto_controle_p.metodo_posicionamento
+#         INNER JOIN dominios.referencial_altim 
+#             ON dominios.referencial_altim.code = bpc.ponto_controle_p.referencial_altim
+#         INNER JOIN dominios.sistema_geodesico 
+#             ON dominios.sistema_geodesico.code = bpc.ponto_controle_p.sistema_geodesico
+#         INNER JOIN dominios.orbita 
+#             ON dominios.orbita.code = bpc.ponto_controle_p.orbita
+#         INNER JOIN dominios.referencia_medicao_altura 
+#             ON dominios.referencia_medicao_altura.code = bpc.ponto_controle_p.referencia_medicao_altura
+#         WHERE cod_ponto = %s
+#     """
